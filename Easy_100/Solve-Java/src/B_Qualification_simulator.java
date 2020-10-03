f9c56e5d1a6c42056f609a857860c234e465933e
Izumo <devwhbwr@gmail.com>
2020-10-04 08:17:50 +0900

    Solve 2. B-Power Socket 3. C.Rally

---
+++ Easy_100/Solve-Java/src/B_Qualification_simulator.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class B_Qualification_simulator {
+    public static void run(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N*2);
+    }
+}
---
+++ Easy_100/SolveCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SolveCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
