540d1e56a3d27fd4bccd62ac1a41247054370329
Izumo <devwhbwr@gmail.com>
2020-10-04 14:50:49 +0900

    #3 B. Qualification simulator

--- AtCoderProblemsBootcampEasy100/QualificationSimulator/Program.cs
+++ AtCoderProblemsBootcampEasy100/QualificationSimulator/Program.cs
@@ -6,7 +6,54 @@ namespace QualificationSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Input
+            string nab = Console.ReadLine();
+            string s = Console.ReadLine();
+            string[] arynab = nab.Split(" ");
+
+            int a = int.Parse(arynab[1]);
+            int b = int.Parse(arynab[2]);         
+            int judgeAplusB = a + b;
+            int passCount = 0;
+            int internationalOrder = 0;
+
+            bool judge = false;
+
+            foreach(var item in s)
+            {
+                //cのその他は無条件で脱落
+                if (item.ToString() == "c") judge = false;
+               
+     
+                if (item.ToString() == "a")
+                {
+                    judge = passCount < judgeAplusB ? true : false;
+                    if (judge) passCount++;
+                }
+
+                if (item.ToString() == "b")
+                {    
+                    if ((passCount < judgeAplusB) && (internationalOrder < b)) {
+                        judge = true;
+                        passCount++;
+                        internationalOrder++;
+                    }
+                    else
+                    {
+                        judge = false;
+                    }
+                }               
+
+                //Output Result
+                if (judge)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
         }
     }
 }
